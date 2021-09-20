4058f150502e10d29e5f0342da322e214e3bea36
yuko-kochi <happy.k165131@gmail.com>
2021-09-20 11:08:58 +0000

    [Finish]7.4

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -14,4 +14,20 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert'
     assert_select 'form[action="/signup"]'
   end
+  
+    test "valid signup information" do
+    get signup_path
+    # assert_differenceブロック内の処理を実行する直前と、実行した直後のUser.countの値を比較
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    # follow_redirect!というメソッドは、POSTリクエストを送信した結果を見て、指定されたリダイレクト先に移動するメソッド
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+  
 end

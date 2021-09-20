b146d38dc4e6258c5954a4da7c65733eafeff5c5
yuko-kochi <happy.k165131@gmail.com>
2021-09-20 11:50:01 +0000

    [Finish]8.1

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    # downcaseメソッドを使って、有効なメールアドレスが入力されたときに確実にマッチするようにする
+    user = User.find_by(email: params[:session][:email].downcase)
+    # ユーザーがデータベースにあり、かつ、認証に成功した場合にのみ
+    # authenticateメソッドパスワードを引数としてユーザーの認証を行うことができる
+    if user && user.authenticate(params[:session][:password])
+    else
+      # flash.nowのメッセージはその後リクエストが発生したときに消滅
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
-  resources :users
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
 end

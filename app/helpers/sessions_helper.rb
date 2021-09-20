e41f7241a80d87e7dc376de1543b2d611a11f52f
yuko-kochi <happy.k165131@gmail.com>
2021-09-20 12:46:59 +0000

    [Finish]8_Implement basic login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,4 +15,10 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # 現在のユーザーをログアウトする
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -20,6 +20,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get login_path
     post login_path, params: { session: { email:    @user.email,
                                           password: 'password' } }
+    assert is_logged_in?
     # リダイレクト先が正しいかどうかをチェック
     assert_redirected_to @user
     follow_redirect!
@@ -29,6 +30,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+
+    delete logout_path
+    # assert_notはfalseなら成功となる
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 
 end

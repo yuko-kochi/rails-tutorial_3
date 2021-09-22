9779e54a943bc49697ccd426959047bfb00dbedb
yuko-kochi <happy.k165131@gmail.com>
2021-09-22 10:38:06 +0000

    [Finish]9_Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,6 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      remember user
       redirect_to user
     else
       # flash.nowのメッセージはその後リクエストが発生したときに消滅

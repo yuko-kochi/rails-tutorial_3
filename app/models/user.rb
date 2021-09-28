1fe1177d426d4704d053df60e4ac4df5bac11583
yuko-kochi <happy.k165131@gmail.com>
2021-09-28 07:13:55 +0000

    [Finish]11.1_AccountActivationsリソース

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
 
   # before_saveというコールバック
   # ユーザーをデータベースに保存する前にemail属性を強制的に小文字に変換
@@ -49,5 +51,17 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  private
+    # メールアドレスをすべて小文字にする
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # 有効化トークンとダイジェストを作成および代入する
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 
 end
